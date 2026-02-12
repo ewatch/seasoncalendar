17dd5af5e9a804195d0e06b4fd349d9b3e2696bb
ewatch <ewatchhu@gmx.de>
2026-02-12 21:06:35 +0100

    refine seasonal page design for mobile and current month

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,8 +7,23 @@ module.exports = {
 			fontFamily: {
 				/** Change here the font family */
 				sans: ["Inter Variable", "Inter", ...defaultTheme.fontFamily.sans],
+				serif: ["Georgia", "serif"],
 			},
 			colors: {
+				paper: "#FDFCF8",
+				stone: {
+					50: "#fafaf9",
+					100: "#f5f5f4",
+					200: "#e7e5e4",
+					300: "#d6d3d1",
+					400: "#a8a29e",
+					500: "#78716c",
+					600: "#57534e",
+					700: "#44403c",
+					800: "#292524",
+					900: "#1c1917",
+					950: "#0c0a09",
+				},
 				/** Override here your primary colors */
 				primary: {
 					50: "#fef1f7",
